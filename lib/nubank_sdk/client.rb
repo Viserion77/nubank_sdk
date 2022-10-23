25fbf00886f86b138637f58b69436496333fe78c
Viserion77 <jeferson.a.oficial@gmail.com>
2022-10-22 22:10:45 -0300

    feat(client): :sparkles: created interfaces to http and https connections

--- lib/nubank_sdk/client.rb
+++ lib/nubank_sdk/client.rb
@@ -1,11 +1,51 @@
 # frozen_string_literal: true
 
+require 'faraday'
 require 'json'
 
 module NubankSdk
-  class Client
+  module Client
     def self.get_body(response)
       JSON.parse(response.body, symbolize_names: true)
+    end
+
+    class HTTP
+      def initialize(base_url, adapter = nil)
+        @connection = Faraday.new(url: base_url) do |faraday|
+          faraday.adapter *adapter if adapter
+          faraday.adapter Faraday.default_adapter unless adapter
+        end
+      end
+
+      def post(path, body)
+        @connection.post(path) do |req|
+          req.headers['Content-Type'] = 'application/json'
+          req.body = body.to_json
+        end
+      end
+
+      def get(path)
+        @connection.get(path)
+      end
+    end
+
+    class HTTPS
+      def initialize(certificate, adapter = nil)
+        client_cert = OpenSSL::X509::Certificate.new(certificate.certificate),
+        client_key = OpenSSL::PKey::RSA.new(certificate.key)
+
+        @connection = Faraday.new(ssl: { client_cert: client_cert, client_key: client_key}) do |faraday|
+          faraday.adapter *adapter if adapter
+          faraday.adapter Faraday.default_adapter unless adapter
+        end
+      end
+
+      def post(url, body)
+        @connection.post(url) do |req|
+          req.headers['Content-Type'] = 'application/json'
+          req.body = body.to_json
+        end
+      end
     end
   end
 end
--- spec/nubank_sdk/client_spec.rb
+++ spec/nubank_sdk/client_spec.rb
@@ -12,4 +12,77 @@ RSpec.describe NubankSdk::Client do
       expect(body).to eq({ foo: 'bar' })
     end
   end
+
+  context 'HTTP' do
+    let(:base_url) { 'http://example.com' }
+    let(:stubs) { Faraday::Adapter::Test::Stubs.new }
+    
+    subject { described_class::HTTP.new(base_url, [:test, stubs]) }
+
+    describe '#post' do
+      it "posts a request to the given path" do
+        stubs.post('/foo') { [200, {}, { foo: 'bar' }.to_json] }
+
+        response = subject.post('/foo', { foo: 'bar' })
+
+        expect(response.status).to eq(200)
+      end
+    end
+
+    describe '#get' do
+      it "gets a request to the given path" do
+        stubs.get('/foo') { [200, {}, { foo: 'bar' }.to_json] }
+
+        response = subject.get('/foo')
+
+        expect(response.status).to eq(200)
+      end
+    end
+  end
+
+  context 'HTTPS' do
+    let(:key) { OpenSSL::PKey::RSA.new 2048 }
+    let(:cpf) { '12345678909' }
+    let(:certificate) do
+      clear_certifications_folder
+      dummy_certification = OpenSSL::X509::Certificate.new.tap do |cert|
+        cert.version = 2
+        cert.serial = 0
+        cert.subject = OpenSSL::X509::Name.parse '/CN=example.com'
+        cert.issuer = cert.subject
+        cert.public_key = key.public_key
+        cert.not_before = Time.now
+        cert.not_after = Time.now + 3600
+        ef = OpenSSL::X509::ExtensionFactory.new
+        ef.subject_certificate = cert
+        ef.issuer_certificate = cert
+        cert.add_extension(ef.create_extension('basicConstraints', 'CA:TRUE', true))
+        cert.add_extension(ef.create_extension('subjectKeyIdentifier', 'hash'))
+        cert.sign(key, OpenSSL::Digest::SHA256.new)
+      end
+      certification = NubankSdk::Certificate.new(cpf, key)
+      certification.process_decoded(dummy_certification)
+      certification.encoded
+    end
+    let(:certificate_path) { "#{NubankSdk::Certificate::FILES_PATH}#{cpf}.p12" }
+    let(:stubs) { Faraday::Adapter::Test::Stubs.new }
+
+    subject { described_class::HTTPS.new(certificate, [:test, stubs]) }
+
+    describe '#post' do
+      it "posts a request to the given path" do
+        stubs.post('/foo') { [200, {}, { foo: 'bar' }.to_json] }
+
+        response = subject.post('/foo', { foo: 'bar' })
+
+        expect(response.status).to eq(200)
+      end
+    end
+
+    after { clear_certifications_folder }
+  
+    def clear_certifications_folder
+      File.delete(certificate_path) if File.exist?(certificate_path)
+    end
+  end
 end
